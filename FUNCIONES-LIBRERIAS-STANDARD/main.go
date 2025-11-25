d589f92e71a583be9729f2f856dc95c80c042708
DavidQRy <davidgui393@gmail.com>
2025-11-24 20:25:12 -0500

    Refactor: add higher-order functions, function parameters, and anonymous functions

--- FUNCIONES-LIBRERIAS-STANDARD/main.go
+++ FUNCIONES-LIBRERIAS-STANDARD/main.go
@@ -1,38 +1,42 @@
 package main
 
-func sum(a int, b int) int {
-	return a + b
+import "fmt"
+
+type Operation func(int, int) int
+
+func sum(a int, b int) int      { return a + b }
+func subtract(a int, b int) int { return a - b }
+func multiply(a int, b int) int { return a * b }
+func divide(a int, b int) int   { return a / b }
+
+func presentResult(a int, b int, op Operation, name string) {
+	fmt.Println(name, "=>", op(a, b))
 }
 
-func subtract(a int, b int) int {
-	return a - b
-}
-
-func multiply(a int, b int) int {
-	return a * b
-}
-
-func divide(a int, b int) int {
-	return a / b
-}
-
-func presentResult(a int, b int, operation string) {
-	if operation == "+" {
-		println("The sum of ", a, " + ", b, " is: ", sum(a, b))
-	} else if operation == "-" {
-		println("The subtraction of ", a, " - ", b, " is: ", subtract(a, b))
-	} else if operation == "*" {
-		println("The multiplication of ", a, " * ", b, " is: ", multiply(a, b))
-	} else if operation == "/" {
-		println("The division of ", a, " / ", b, " is: ", divide(a, b))
-	} else {
-		println("Invalid operation")
+func getOperation(op string) Operation {
+	if op == "+" {
+		return sum
+	} else if op == "-" {
+		return subtract
+	} else if op == "*" {
+		return multiply
+	} else if op == "/" {
+		return divide
 	}
+	return func(a, b int) int { return 0 }
 }
 
 func main() {
-	presentResult(5, 6, "+")
-	presentResult(3, 9, "-")
-	presentResult(2, 5, "*")
-	presentResult(10, 2, "/")
+	presentResult(5, 6, sum, "Sum")
+	presentResult(3, 9, subtract, "Subtraction")
+	presentResult(2, 5, multiply, "Multiplication")
+	presentResult(10, 2, divide, "Division")
+
+	op := getOperation("*")
+	fmt.Println("Using getOperation:", op(7, 3))
+
+	func() {
+		result := sum(4, 6)
+		fmt.Println("Inner anonymous function result:", result)
+	}()
 }
